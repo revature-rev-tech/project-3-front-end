9c303a3f9dcf5549d2cbdb884ab6aa8d4ca1e7a8
jldroid25 <jldroid25@protonmail.com>
2021-12-23 00:22:14 -0500

    pushing fontend changes for product component & enndpoints connected to table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StoreProductsComponent } from './products/store-products/store-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products', component: StoreProductsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { StoreProductsComponent } from './products/store-products/store-products.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreProductsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
