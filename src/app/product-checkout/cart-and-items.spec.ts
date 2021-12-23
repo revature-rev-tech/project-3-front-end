f3b2db93f78579d65cf8ed6b3590ff89a1377767
aimenAlt <ymnghost@gmail.com>
2021-12-23 13:26:15 -0800

    added models to product-checkout

---
+++ src/app/product-checkout/cart-and-items.spec.ts
@@ -0,0 +1,7 @@
+import { CartAndItems } from './cart-and-items.model';
+
+describe('CartAndItems', () => {
+  it('should create an instance', () => {
+    expect(new CartAndItems()).toBeTruthy();
+  });
+});
