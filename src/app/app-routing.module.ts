ea2e1b41d7676bd0923a09747fbb6cfaa6c8c812
Michelle Harper <michelle.ym.harper@gmail.com>
2021-12-23 13:55:37 -0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { HelloAdminComponent } from './hello/hello-admin/hello-admin.component';
+import { HelloCustomerComponent } from './hello/hello-customer/hello-customer.component';
+import { StoreProductComponent } from './product/store-product/store-product.component';
+import { AdminGuard } from './users/admin.guard';
+import { AdminComponent } from './users/admin/admin.component';
+import { CustomerComponent } from './users/customer/customer.component';
+import { LoginComponent } from './users/login/login.component';
+import { LogoutComponent } from './users/logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent},
+  { path: "logout", component: LogoutComponent},
+  { path: "hello-admin", component: HelloAdminComponent, canActivate: [AdminGuard]},
+  { path: "hello-customer", component: HelloCustomerComponent, canActivate: [AdminGuard]},
+  { path: "admin", component: AdminComponent, canActivate: [AdminGuard]},
+  { path: "customer", component: CustomerComponent, canActivate: [AdminGuard]},
+  { path: "store-product", component: StoreProductComponent, canActivate: [AdminGuard]},
+  { path: "checkout", component: CheckoutComponent, canActivate: [AdminGuard]}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: true
+    })
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { StoreProductComponent } from './product/store-product/store-product.component';
+import { HelloAdminComponent } from './hello/hello-admin/hello-admin.component';
+import { HelloCustomerComponent } from './hello/hello-customer/hello-customer.component';
+import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './users/admin/admin.component';
+import { CustomerComponent } from './users/customer/customer.component';
+import { LoginComponent } from './users/login/login.component';
+import { LogoutComponent } from './users/logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckoutComponent,
+    StoreProductComponent,
+    HelloAdminComponent,
+    HelloCustomerComponent,
+    HeaderComponent,
+    AdminComponent,
+    CustomerComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
