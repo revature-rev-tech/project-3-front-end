a57e3d2a9151e09058b4233514eefd2ab9466282
aimenAlt <ymnghost@gmail.com>
2021-12-30 12:23:32 -0800

    undo changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,19 +10,10 @@ import { AdminComponent } from './users/admin/admin.component';
 import { CustomerComponent } from './users/customer/customer.component';
 import { LoginComponent } from './users/login/login.component';
 import { LogoutComponent } from './users/logout/logout.component';
-import { CheckoutComponent } from './product-checkout/checkout/checkout.component';
+import {ProductPageComponent} from "./product-checkout/product-page/product-page.component";
 
 
 const routes: Routes = [
-<<<<<<< HEAD
-  { path: "login", component: LoginComponent },
-  { path: "logout", component: LogoutComponent },
-  { path: "hello-admin", component: HelloAdminComponent, canActivate: [AdminGuard] },
-  { path: "hello-customer", component: HelloCustomerComponent, canActivate: [AdminGuard] },
-  { path: "admin", component: AdminComponent, canActivate: [AdminGuard] },
-  { path: "customer", component: CustomerComponent, canActivate: [AdminGuard] },
-  { path: "checkout", component: CheckoutComponent },
-=======
   { path: "login", component: LoginComponent},
   { path: "logout", component: LogoutComponent},
   { path: 'product', component: StoreProductComponent },
@@ -30,7 +21,7 @@ const routes: Routes = [
   { path: "hello-customer", component: HelloCustomerComponent, canActivate: [AdminGuard]},
   { path: "admin", component: AdminComponent, canActivate: [AdminGuard]},
   { path: "customer", component: CustomerComponent, canActivate: [AdminGuard]},
->>>>>>> a53744ac087b93f54a098354ff51c5c8dd1d28f8
+  { path: "product-page", component: ProductPageComponent, canActivate: [AdminGuard]},
 ];
 
 @NgModule({
@@ -43,5 +34,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
